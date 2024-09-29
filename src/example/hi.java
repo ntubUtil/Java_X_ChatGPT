20f763fffe9e73dd396255fb45f3382fead2c61d
yaodehaung <sef1548@gmail.com>
2024-09-29 08:01:12 +0800

    init

---
+++ src/example/hi.java
@@ -0,0 +1,18 @@
+package example;
+
+import java.util.*;
+
+public class hi {
+	
+	public static void main(String[] args) {
+
+		Scanner scn = new Scanner(System.in);
+		System.out.println("ex01 Please Enter name:");
+		String namee = scn.next();
+		System.out.println("HI" + namee  + "Welcome Java");
+		scn.close();
+			
+	}
+	
+
+}
---
+++ src/example/mix.java
@@ -0,0 +1,10 @@
+package example;
+
+public class mix {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
